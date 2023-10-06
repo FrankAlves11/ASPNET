ee3596ef303543e4227bb19979c3609b6ee2bda0
FrankAlves11 <Frankie.alves@icloud.com>
2023-10-06 08:07:57 -0700

    made changes

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+                
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("Select * From products");
+
+        }
+
+        public Product GetProduct(int id)
+        {       
+            
+                return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
+            
+        }
+    }
+}
