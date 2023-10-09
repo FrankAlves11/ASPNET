913bd13586d6d265eae27f2f5afac9e1fc920572
FrankAlves11 <Frankie.alves@icloud.com>
2023-10-09 12:55:51 -0700

    made changes

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Testing.Models
 {
@@ -10,5 +11,7 @@ namespace Testing.Models
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
     }
 }
